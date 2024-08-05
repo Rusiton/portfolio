47a687188ac24b7d7eeb8e1f02f2888dcebeede7
Rusiton <galassosantiago05@gmail.com>
2024-08-05 18:58:24 -0300

    Fixed theme button links

--- src/static/js/headerControl.js
+++ src/static/js/headerControl.js
@@ -5,8 +5,8 @@ themeContainer.addEventListener("click", e => {
 
     let body = document.querySelector("body");
 
-    if(body.className == "theme-dark"){ e.target.src = "http://mattprofe.com.ar:81/alumno/11994/personal/portfolio/src/static/img/sun.svg"; }
-    else{ e.target.src = "http://mattprofe.com.ar:81/alumno/11994/personal/portfolio/src/static/img/moon.svg"; }
+    if(body.className == "theme-dark"){ e.target.src = "src/static/svg/sun.svg"; }
+    else{ e.target.src = "src/static/svg/moon.svg"; }
 
     if(body.className == "theme-dark"){
         body.className = "theme-light";
